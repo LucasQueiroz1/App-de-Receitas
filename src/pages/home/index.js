a096596ebf5cd5740032d17a8110c3479983b375
Lucas Queiroz <lucasqueiroz34@gmail.com>
2023-04-30 20:21:51 -0300

    App do Workshop finalizado

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -10,6 +10,8 @@ import {
 } from "react-native";
 
 import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { Text as MotiText } from "moti";
 
 import { Logo } from "../../components/logo";
 import { FoodList } from "../../components/foodlist";
@@ -18,6 +20,8 @@ import api from "../../services/api";
 export function Home() {
   const [inputValue, setInputValue] = useState("");
   const [foods, setFoods] = useState([]);
+
+  const navigation = useNavigation();
 
   useEffect(() => {
     async function fetchApi() {
@@ -29,16 +33,53 @@ export function Home() {
   }, []);
 
   function handleSearch() {
-    console.log("Voce digitou:");
-    console.log(inputValue);
+    if (!inputValue) return;
+
+    let input = inputValue;
+    setInputValue("");
+    navigation.navigate("Search", { name: input });
   }
 
   return (
     <SafeAreaView style={styles.container}>
       <Logo />
 
-      <Text style={styles.title}>Encontre a receita</Text>
-      <Text style={styles.title}>que combina com você</Text>
+      <MotiText
+        style={styles.title}
+        from={{
+          opacity: 0,
+          translateY: 15,
+        }}
+        animate={{
+          opacity: 1,
+          translateY: 0,
+        }}
+        transition={{
+          delay: 100,
+          type: "timing",
+          duration: 650,
+        }}
+      >
+        Encontre a receita
+      </MotiText>
+      <MotiText
+        style={styles.title}
+        from={{
+          opacity: 0,
+          translateY: 18,
+        }}
+        animate={{
+          opacity: 1,
+          translateY: 0,
+        }}
+        transition={{
+          delay: 200,
+          type: "timing",
+          duration: 850,
+        }}
+      >
+        que combina com você
+      </MotiText>
 
       <View style={styles.form}>
         <TextInput
