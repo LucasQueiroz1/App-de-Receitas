9fb1759aa8b510524e9f4eced34a82060bd19b88
LucasQueiroz1 <lucasqueiroz34@gmail.com>
2023-09-11 10:36:51 -0300

    falta mudar renderização das receitas

--- src/services/api.js
+++ src/services/api.js
@@ -2,11 +2,11 @@ import axios from "axios";
 
 /*
 // Rodar com IPV4, comando para utilizar em um terminal secundário:
-    json-server --watch -d {Dellay de resposta} 180 --host {IPV4} 192.168.0.13 db.json
+    json-server --watch -d {Dellay de resposta} 180 --host {IPV4} 192.168.0.10 db.json
 */
 
 const api = axios.create({
-  baseURL: "http://192.168.0.13:3000/",
+  baseURL: "http://192.168.0.10:3000/",
 });
 
 export default api;
